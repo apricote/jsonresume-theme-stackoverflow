5e25d2ede5154ea999e2ac296a7c65a31fac2f05
Julian Tölle <julian.toelle97@gmail.com>
2022-06-08 16:24:05 +0200

    feat: markdown like paragraph-split
    The new version only creates a new paragraph when there is an empty line in
    the source text, instead of a new paragraph for every new line. This matches
    the behavior of markdown parsers.

--- theme/hbs-helpers/paragraph-split.js
+++ theme/hbs-helpers/paragraph-split.js
@@ -1,9 +1,30 @@
-const { SafeString } = require('handlebars');
+const { SafeString } = require("handlebars");
 
+/**
+ *
+ * @param {string} text
+ * @returns
+ */
 const paragraphSplit = (text) => {
   const expr = /\r\n|\r|\n/g;
-  const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr);
-  const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
+  const lines = Array.isArray(text)
+    ? text.join("").split(expr)
+    : text.split(expr);
+  const output = lines
+    .reduce((combinedLines, line, index, array) => {
+      previousLine =
+        combinedLines.length > 0 ? combinedLines[combinedLines.length - 1] : "";
+
+      if (line === "" || previousLine === "") {
+        combinedLines.push(line);
+      } else {
+        combinedLines[combinedLines.length - 1] = `${previousLine} ${line}`;
+      }
+
+      return combinedLines;
+    }, [])
+    .reduce((a, b) => `${a}<p>${b}</p>`, "");
+
   return new SafeString(output);
 };
 
